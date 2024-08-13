a9e88e76e7c6b60258859db98fe6d24ce3851cf3
JulioCesarTavarez <145470440+JulioCesarTavarez@users.noreply.github.com>
2024-08-13 11:37:49 -0600

    fixing tree

--- road_kill_allert.html/wp_plugin.php
+++ road_kill_allert.html/wp_plugin.php
@@ -2,4 +2,19 @@
 Plugin Name: Road Kill Allert
 plugin URI: www.Roadkillalert.com -->
 
+
 <?php
+$servername = "localhost";
+$username = "3io";
+$password = "Bigdawg13$$$";
+
+
+// Create connection
+$conn = new mysqli($servername, $username, $password);
+
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+echo "Connected successfully";
+?>
