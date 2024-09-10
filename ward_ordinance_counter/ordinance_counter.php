aa51f5e424a52dabab7aa64292be1da31d5e3fc3
JulioCesarTavarez <145470440+JulioCesarTavarez@users.noreply.github.com>
2024-09-10 11:57:37 -0600

    making a goal tracker

---
+++ ward_ordinance_counter/ordinance_counter.php
@@ -0,0 +1,8 @@
+<?php
+
+$dbServername = "localhost";
+$dbUsername = "root";
+$dbPassword = "Bigdawg13$$$";
+$dbName = "group_goal";
+
+$conn = mysqli_connect( $dbServername , $dbUsername , $dbPassword , $dbName );
